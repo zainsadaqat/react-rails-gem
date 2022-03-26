9a40c0682fce82fb6f313bd2c5cfe611d59d25ba
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-27 00:09:34 +0500

    Create app component

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,10 +1,16 @@
-var App = createReactClass({
+const { BrowserRouter } = require('react-router-dom');
 
-  render: function() {
+var App = createReactClass({
+  render: function () {
     return (
       <React.Fragment>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="" render={() => 'Hello'} />
+            <Route exact path="/greeting" render={<Greeting />} />
+          </Switch>
+        </BrowserRouter>
       </React.Fragment>
     );
-  }
+  },
 });
-
