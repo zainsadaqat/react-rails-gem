8f88f0379335575f398e869f6b6c42d7672c96df
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-27 00:25:21 +0500

    Add store in App Comp. using Provider

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,15 +1,18 @@
+const { Provider } = require('react-redux');
 const { BrowserRouter } = require('react-router-dom');
 
 var App = createReactClass({
   render: function () {
     return (
       <React.Fragment>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="" render={() => 'Hello'} />
-            <Route exact path="/greeting" render={<Greeting />} />
-          </Switch>
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="" render={() => 'Hello'} />
+              <Route exact path="/greeting" render={<Greeting />} />
+            </Switch>
+          </BrowserRouter>
+        </Provider>
       </React.Fragment>
     );
   },
